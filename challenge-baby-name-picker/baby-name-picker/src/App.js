dad50f508fe732c7a0c2610f3a2c4ce135b19398
Aycin Kaya <aycin@Aycins-MacBook-Pro-2.local>
2023-09-27 13:11:42 +0100

    added a search bar which updates the names list based on user input

--- challenge-baby-name-picker/baby-name-picker/src/App.js
+++ challenge-baby-name-picker/baby-name-picker/src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import babyNamesData from "./../src/babyNamesData.json";
-import react from "react";
+import react, { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [babyNames, setBabyNames] = useState(babyNamesData);
+
+  useEffect(() => {
+    refineSearch(input);
+  }, [input]);
+
   function mapBabyNames() {
-    return Object.values(babyNamesData).map((baby, i) => {
+    return Object.values(babyNames).map((baby, i) => {
       return (
         <button
           key={i}
@@ -27,8 +35,33 @@ function App() {
     }
   }
 
+  function searchBar() {
+    return (
+      <input
+        placeholder="Enter name here"
+        onChange={handleChange}
+        type="text"
+      ></input>
+    );
+  }
+
+  function handleChange(e) {
+    setInput(e.target.value);
+  }
+
+  function refineSearch() {
+    const filteredNames = Object.values(babyNamesData).filter(checkInput);
+    return setBabyNames(filteredNames);
+  }
+
+  function checkInput(baby) {
+    const isIncluded = baby.name.toLowerCase().includes(input);
+    return isIncluded;
+  }
+
   return (
     <div className="App">
+      <div className="searchBar">{searchBar()}</div>
       <div className="babyButtons">{mapBabyNames()}</div>
     </div>
   );
